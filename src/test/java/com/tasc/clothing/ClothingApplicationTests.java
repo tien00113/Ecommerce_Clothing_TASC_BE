d4d9873f375297c91af7e62b515f2a657929f2ad
Nguyen Hien Tien <nht010103@gmail.com>
2024-05-23 12:44:52 +0700

    created Project

---
+++ src/test/java/com/tasc/clothing/ClothingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasc.clothing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClothingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
